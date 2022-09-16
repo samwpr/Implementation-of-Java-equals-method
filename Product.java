89fcd49c64d51c3c58b906034b1f02d66e5e0390
samwpr <swpr10x@gmail.com>
2022-09-16 12:11:27 +0800

    implementation for equals & hashcode method

--- Product.java
+++ Product.java
@@ -1,4 +1,7 @@
-// https://www.sitepoint.com/implement-javas-equals-method-correctly/
+// Best explanation: https://www.baeldung.com/java-equals-hashcode-contracts
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
 
 class Product {
     private double price;
@@ -21,18 +24,39 @@ class Product {
         return description;
     }
 
-    public boolean equalsW(Product someProduct){
-        return Object.equalsW(price, someProduct.price) && Object.equalsW(description, someProduct.description);
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj){
+            return true;
+        }
+        if(obj == null){
+            return false;
+        }
+        if(obj instanceof Product){
+            Product other = (Product) obj;
+            if(other.price == this.price && other.description == this.description){
+                return true;
+            }
+        }
+        return false;
     }
+
+    @Override
+    public int hashCode(){
+        int hash = 17;
+        hash = 31 * hash + Double.valueOf(price).hashCode();
+        hash = 31 * hash + description.hashCode();
+        return hash;
+    }
+
 
     public static void main(String[] args) {
         Product watch1 = new Product(69.99, "Apple watch 1");
         Product watch2 = new Product(69.99, "Apple watch 1");
-
-        boolean equal = watch1.equals(watch2);
-
-
-
+        System.out.println(watch1.equals(watch2));
+        System.out.println(watch1.hashCode());
+        System.out.println(watch2.hashCode());
     }
     
 }
